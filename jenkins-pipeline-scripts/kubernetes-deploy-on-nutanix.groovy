ab3f812a096e978d84eab172a65763d969d5efad
12345pradeep <pfageria@calance.com>
2022-12-27 17:19:29 +0530

    added jenkins pipeline script to calance workflows

--- jenkins-pipeline-scripts/kubernetes-deploy-on-nutanix.groovy
+++ jenkins-pipeline-scripts/kubernetes-deploy-on-nutanix.groovy
@@ -1,46 +1,47 @@
-def imageName = ""
-def cleanedReleaseName = ""
+def imageName = ''
+def cleanedReleaseName = ''
 
-node("master") {
-    stage("Identify Cluster for deployment") {
+node('master') {
+    stage('Identify Cluster for deployment') {
         configFileProvider([
-            configFile(fileId: "service-cluster-configuration.json", variable: 'CONFIG_FILE')
+            configFile(fileId: 'service-cluster-configuration.json', variable: 'CONFIG_FILE')
         ]) {
             def data = readJSON file: "$CONFIG_FILE"
             cloudName = data["$SERVICE_NAME-$CLUSTER_ENVIRONMENT"]
 
-            if (cloudName == null || cloudName == "") {
+            if (cloudName == null || cloudName == '') {
                 error "Cluster config does not exist for $SERVICE_NAME-$CLUSTER_ENVIRONMENT"
             }
 
             print "For application - $SERVICE_NAME, environment - $CLUSTER_ENVIRONMENT, Chosen cloud - $cloudName"
-            
+
             // IMAGE_NAME param was added later, so for old workflows
             // to work we get image name from SERVICE_NAME param
             imageName = IMAGE_NAME
-            if (imageName == null || imageName == "") {
+            if (imageName == null || imageName == '') {
                 imageName = SERVICE_NAME
             }
             print "Image Name -> $imageName"
             
             // RELEASE_NAME param was added later, so for old workflows
             // to work we get release name from IMAGE_NAME or SERVICE_NAME params
+
             def releaseName = RELEASE_NAME
-            if (releaseName == null || releaseName == "") {
+            if (releaseName == null || releaseName == '') {
                 releaseName = IMAGE_NAME
             }
-            if (releaseName == null || releaseName == "") {
+            if (releaseName == null || releaseName == '') {
                 releaseName = SERVICE_NAME
             }
             // helm release names can contain only alphanumeric and hyphen characters
-            cleanedReleaseName = releaseName.toLowerCase().replaceAll("[-_\\s]+","-").replaceAll("[^a-z0-9-]", "")
+            cleanedReleaseName = releaseName.toLowerCase().replaceAll('[-_\\s]+', '-').replaceAll('[^a-z0-9-]', '')
             print "Release Name -> $cleanedReleaseName"
         }
     }
 }
 node("$cloudName") {
     container('helm') {
-        stage("Logging into registry") {
+        stage('Logging into registry') {
             sh '''
                 set +x
                 helm registry login -u $GITHUB_USERNAME -p $GITHUB_PASSWORD ghcr.io
@@ -48,7 +49,7 @@ node("$cloudName") {
             '''
         }
 
-        stage("Deploying application") {
+        stage('Deploying application') {
             def valuesFilename = "${cleanedReleaseName}-${CLUSTER_ENVIRONMENT}"
             print "For release $cleanedReleaseName, picking values from config file - $valuesFilename"
 
