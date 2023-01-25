f1f9d20102a95d8c1380dd6f3ac9ab998d74c42f
hsachan22 <hsachan@calance.com>
2023-01-25 11:49:30 +0530

    eslint issue fix #1

--- actions/cluster-env-setter/index.js
+++ actions/cluster-env-setter/index.js
@@ -1,22 +1,22 @@
-const core = require('@actions/core');
+const core = require('@actions/core')
 
 try {
-  const dockerJsonMetadata = core.getInput('docker_json_metadata');
-  const dockerMetadata = JSON.parse(dockerJsonMetadata);
+  const dockerJsonMetadata = core.getInput('docker_json_metadata')
+  const dockerMetadata = JSON.parse(dockerJsonMetadata)
 
-  const imageVersion = dockerMetadata.labels['org.opencontainers.image.version'];
+  const imageVersion = dockerMetadata.labels['org.opencontainers.image.version']
 
-  if(!imageVersion || typeof imageVersion !== 'string') {
+  if (!imageVersion || typeof imageVersion !== 'string') {
     throw new Error('Unable to get image version from metadata')
   }
 
-  const isTesting = imageVersion.match(/.*-rc[0-9]/);
+  const isTesting = imageVersion.match(/.*-rc[0-9]/)
 
-  if(isTesting !== null) {
+  if (isTesting !== null) {
     core.setOutput('cluster_environment', 'testing')
   } else {
     core.setOutput('cluster_environment', 'production')
   }
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
