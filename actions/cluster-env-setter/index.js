8376ccedb93719085e83ef2edec4afc222df88a2
Hardik Sachan <hsachan@calance.com>
2023-02-17 18:39:19 +0530

    174 adding custom action for setting cluster_environment (#19)

---
+++ actions/cluster-env-setter/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core')
+
+try {
+  const dockerJsonMetadata = core.getInput('docker_json_metadata')
+  const dockerMetadata = JSON.parse(dockerJsonMetadata)
+
+  const imageVersion = dockerMetadata.labels['org.opencontainers.image.version']
+
+  if (!imageVersion || typeof imageVersion !== 'string') {
+    throw new Error('Unable to get image version from metadata.')
+  }
+
+  const isProduction = imageVersion.match(/^v\d+\.\d+\.\d+$/)
+  const isTesting = imageVersion.match(/^v\d+\.\d+\.\d+-rc\d+$/)
+
+  if (isProduction) {
+    core.setOutput('cluster_environment', 'production')
+  } else if (isTesting) {
+    core.setOutput('cluster_environment', 'testing')
+  } else {
+    throw new Error('Invalid image version provided.')
+  }
+} catch (error) {
+  core.setFailed(error.message)
+}
