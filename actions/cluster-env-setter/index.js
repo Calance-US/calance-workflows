7752def65d55fdc43ed7b8636e29f13976a39ffe
12345pradeep <108867066+12345pradeep@users.noreply.github.com>
2023-04-25 14:46:36 +0530

    change-env-setter-action-according-to-new-dev-enviornment-in-moneyquiz (#36)

--- actions/cluster-env-setter/index.js
+++ actions/cluster-env-setter/index.js
@@ -6,17 +6,29 @@ try {
 
   const imageTag = dockerMetadata.labels['org.opencontainers.image.version']
 
+  const repository =  dockerMetadata.labels['org.opencontainers.image.source']
+
   if (!imageTag || typeof imageTag !== 'string') {
     throw new Error('Unable to get image version from metadata.')
   }
 
   const isProduction = imageTag.match(/^\d+\.\d+\.\d+$/) || (imageTag === "main")
 
+  const isTesting = imageTag.match(/^\d+\.\d+\.\d+-rc\d+$/)
+
   if (isProduction) {
     core.setOutput('cluster_environment', 'production')
-  } else {
+  } 
+  else if(isTesting){
     core.setOutput('cluster_environment', 'testing')
   }
+  // for money-quiz dev environment
+  else if (repository.includes('money-quiz')) {
+    core.setOutput('cluster_environment','dev')
+  } else {
+    throw new Error('Invalid image tag')
+  }
+
 } catch (error) {
   core.setFailed(error.message)
 }
